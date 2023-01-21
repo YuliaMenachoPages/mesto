bcb13658a6b0ebdf8498dcc7cf4937941a309019
YuliaMenachoPages <yumenachopages@gmail.com>
2023-01-21 20:18:09 +0300

    version_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,51 @@
-let popup=document.querySelector('.popup');
-let popupOn=document.querySelector('.profile__edit-button');
-let popupOff=document.querySelector('.popup__close');
-let popupSaveButton=document.querySelector('.popup__button');
+//  Popup
+let popup = document.querySelector('.popup');
 
+// Переменные открытия Popup
+let popupOn = document.querySelector('.profile__edit-button');
 
-popupOn.addEventListener('click', function openPopup() {
+// Переменные закрытия Popup
+let popupOff = document.querySelector('.popup__close');
+
+// Переменные обработки формы Submit
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__form-field_type_fullname');
+let jobInput = document.querySelector('.popup__form-field_type_about');
+
+//Открытие Popup
+function openPopup() {
     popup.classList.add('popup_opened');
-})
-popupOff.addEventListener('click', function closePopup() {
-    popup.classList.remove('popup_opened');
-})
-popupSaveButton.addEventListener('click', function closePopup() {
-    popup.classList.remove('popup_opened');
-})
+}
 
-let formElement=document.querySelector('.popup__form');
-    let nameInput=document.querySelector('.popup__fullname');
-    let jobInput=document.querySelector('.popup__about');
+popupOn.addEventListener('click', openPopup);
 
-    function handleFormSubmit (evt) {
-        evt.preventDefault();
-        nameInput.getAttribute('value');
-        jobInput.getAttribute('value');
-        let name=document.querySelector('.profile__name');
-        let about=document.querySelector('.profile__about');
-name.textContent = (nameInput.value);
-about.textContent = (jobInput.value);
+//Закрытие Popup
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+popupOff.addEventListener('click', closePopup);
+
+// Обработка формы Submit и закрытия Popup
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Отмена стандартной отправки формы.
+
+    //Перезапись данных
+    nameInput.getAttribute('value');
+    jobInput.getAttribute('value');
+
+    let name = document.querySelector('.profile__name');
+    let about = document.querySelector('.profile__about');
+
+    name.textContent = (nameInput.value);
+    about.textContent = (jobInput.value);
+
+//Закрытие Popup
+    function closePopup() {
+        popup.classList.remove('popup_opened');
     }
-formElement.addEventListener('submit', handleFormSubmit);
+
+    closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit); //Отправка формы и закрытие Popup
